c7b25f9478ebff93f185d04fd818371c7338a08c
SeongkookKIM <ksk8646@naver.com>
2023-04-28 17:17:04 +0900

    LocalStorage

---
+++ Script.js
@@ -0,0 +1,23 @@
+const taskInput = document.querySelector(".task-input input");
+
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo() {
+  todos.forEach((todo, id) => {
+    console.log(todo, id);
+  });
+}
+
+taskInput.addEventListener("keyup", (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key == "Enter" && userTask) {
+    if (!todos) {
+      todos = [];
+    }
+    taskInput.value = "";
+    let taskInfo = { name: userTask, status: "pending" };
+    todos.push(taskInfo);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
+  }
+});
