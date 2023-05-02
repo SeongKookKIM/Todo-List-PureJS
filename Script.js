6178da72c9c1c1a9e420be85debe7a46853fbb22
SeongkookKIM <ksk8646@naver.com>
2023-05-02 12:24:36 +0900

    Todo-List

--- Script.js
+++ Script.js
@@ -1,37 +1,76 @@
 const taskInput = document.querySelector(".task-input input");
+const filters = document.querySelectorAll(".filters span");
+const clearAll = document.querySelector(".clear-btn");
 const taskBox = document.querySelector(".task-box");
+
+let editId;
+let isEditTask = false;
 
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
-function showTodo() {
+filters.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    btn.classList.add("active");
+    showTodo(btn.id);
+  });
+});
+
+function showTodo(filter) {
   let li = "";
   if (todos) {
-    todos.reverse().forEach((todo, id) => {
+    todos.forEach((todo, id) => {
       let isCompleted = todo.status == "completed" ? "checked" : "";
-      li += `
-            <li class="task">
-            <label for="${id}">
-              <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted} />
-              <p class="${isCompleted}">${todo.name}</p>
-            </label>
-            <div class="settings">
-              <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-              <ul class="task-menu">
-                <li><i class="uil uil-pen"></i>Edit</li>
-                <li><i class="uil uil-trash"></i>Delete</li>
-              </ul>
-            </div>
-          </li>
-            `;
+      if (filter == todo.status || filter == "all") {
+        li += `
+              <li class="task">
+                <label for="${id}">
+                  <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted} />
+                  <p class="${isCompleted}">${todo.name}</p>
+                </label>
+                <div class="settings">
+                  <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                  <ul class="task-menu">
+                    <li onclick="editTask(${id}, '${todo.name}')"><i class="uil uil-pen"></i>Edit</li>
+                    <li onclick="deleteTask(${id})"><i class="uil uil-trash"></i>Delete</li>
+                  </ul>
+                </div>
+              </li>
+                `;
+      }
     });
   }
-  taskBox.innerHTML = li;
+  taskBox.innerHTML = li || `<span>You don't have any task here</span>`;
 }
-showTodo();
+showTodo("all");
 
 function showMenu(selectedTask) {
-  console.log(selectedTask);
+  let taskMenu = selectedTask.parentElement.lastElementChild;
+  taskMenu.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != selectedTask) {
+      taskMenu.classList.remove("show");
+    }
+  });
 }
+
+function editTask(taskId, taskName) {
+  editId = taskId;
+  isEditTask = true;
+  taskInput.value = taskName;
+}
+
+function deleteTask(deleteId) {
+  todos.splice(deleteId, 1);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
+}
+
+clearAll.addEventListener("click", () => {
+  todos.splice(0, todos.length);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
+});
 
 function updateStatus(selectedTask) {
   let taskName = selectedTask.parentElement.lastElementChild;
@@ -48,13 +87,20 @@ function updateStatus(selectedTask) {
 taskInput.addEventListener("keyup", (e) => {
   let userTask = taskInput.value.trim();
   if (e.key == "Enter" && userTask) {
-    if (!todos) {
-      todos = [];
+    if (!isEditTask) {
+      if (!todos) {
+        todos = [];
+      }
+      let taskInfo = { name: userTask, status: "pending" };
+      todos.push(taskInfo);
+    } else {
+      isEditTask = false;
+      todos[editId].name = userTask;
     }
+
     taskInput.value = "";
-    let taskInfo = { name: userTask, status: "pending" };
-    todos.push(taskInfo);
+
     localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo();
+    showTodo("all");
   }
 });
